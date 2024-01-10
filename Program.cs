96e462291b522f5f71529a3cca26994588fd914e
DeFis33 <61848806+DeFis33@users.noreply.github.com>
2024-01-10 12:47:11 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -33,31 +33,61 @@ namespace pr16
                     FileStream F = new FileStream(filepath, FileMode.Create);
                     StreamWriter writer = new StreamWriter(F);
                     Random rnd = new Random();
-                    Console.WriteLine("\nВведите кол-во строк: ");
+                    Console.Write("\nВведите кол-во чисел: ");
                     int M = Int32.Parse(Console.ReadLine());
-                    Console.WriteLine("\nВведите кол-во стобцов: ");
-                    int N = Int32.Parse(Console.ReadLine());
-                    double[,] Mas = new double[M, N];
+                    double[] Mas = new double[M];
 
                     for (int i = 0; i < M; i++)
                     {
-                        for (int j = 0; j < N; j++)
-                        {
-                            Mas[i, j] = (rnd.NextDouble() * 20) - 10; // Генерация числа в интервале [-10, 10]
-
-                            if (j % 2 != 0) product *= Mas[i, j];
-
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.Write(Mas[i, j] + "\n");
-                            Console.ForegroundColor = ConsoleColor.White;
-                            writer.Write(Mas[i, j] + "\n");
-                        }
+                        Mas[i] = (rnd.NextDouble() * 20) - 10; // Генерация числа в интервале [-10, 10]
+                        Mas[i] = Math.Round(Mas[i], 1);
+                        if (i % 2 != 0) product *= Mas[i];
+                        product = Math.Round(product, 1);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(Mas[i] + "\n");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        writer.Write(Mas[i] + "\n");
                     }
                     Console.ForegroundColor = ConsoleColor.DarkGreen;
                     Console.WriteLine("Произведение элементов с нечетными номерами: " + product);
                     Console.ForegroundColor = ConsoleColor.White;
-                    writer.Write("Произведение элементов с нечетными номерами: " + product);
                     writer.Close();
+                }
+                catch (IOException e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ошибка открытия/записи файла. Проверьте месторасположение файла!\n", e.Message);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                catch (FormatException fe)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ошибка формата числа: {0}", fe.Message);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ошибка формата числа: {0}", e.Message);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+                double[] mas = null;
+                string s;
+                try
+                {
+                    FileStream F1 = new FileStream(filepath, FileMode.Open);
+                    StreamReader reader = new StreamReader(F1);
+                    while ((s = reader.ReadLine()) != null) 
+                    {
+                        s = s.TrimEnd(' ');
+                        string[] text = s.Split(' ');
+                        for (int j = 0; j < text.Length; j++)
+                        {
+                            mas[j] = Convert.ToDouble(text[j]);
+                        }
+                    }
+                    F1.Close();
                 }
                 catch (IOException e)
                 {
@@ -82,3 +112,4 @@ namespace pr16
         }
     }
 }
+// нужно, чтобы файл еще и считывался, а потом уже выводил произведение
